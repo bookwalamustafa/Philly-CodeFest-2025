04d1cda25e19653b9433f1cb9dbc178d5c209820
Neil Luong <luongdminh183@gmail.com>
2025-03-01 17:57:24 -0500

    Remove duplicated frontend folder

--- backend/frontend/script.js
+++
@@ -1,6 +0,0 @@
-function convertText() {
-    const fromLang = document.getElementById("fromLanguage").value;
-    const toLang = document.getElementById("toLanguage").value;
-
-    document.getElementById("output").innerHTML = `Converting from <b>${fromLang}</b> to <b>${toLang}</b>...`;
-}
